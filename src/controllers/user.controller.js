0b6e096cbdfe8e0b8b10b2115207d0d487ada77b
Igor Bandero <igorbandero@gmail.com>
2023-08-19 17:58:15 -0300

    feat: search user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { selectSession } from "../repositories/session.repository.js";
 import { getUserPosts } from "../repositories/posts.repository.js";
+import { getUsersBySearchBar } from "../repositories/user.repository.js";
 
 export async function openUserPage(req, res){
 
@@ -19,3 +20,30 @@ export async function openUserPage(req, res){
         res.status(500).send(error.message);
     }
 }
+
+export async function searchUser(req, res){
+
+    const searchTerm = req.query.term;
+    try {
+        const users = await getUsersBySearchBar(searchTerm);
+        const usersList = removeDuplicateUsers(users.rows);
+        res.status(200).send(users.rows);
+    } catch (error) {
+        console.log("Erro ao buscar usuários: ", error.message);
+        res.status(500).send({ message: error.message });
+    };
+}
+
+function removeDuplicateUsers(usersList) {
+    const idSet = new Set(); 
+    const uniqueList = [];  
+    for (const obj of usersList) {
+      if (!idSet.has(obj.id)) {
+        idSet.add(obj.id);
+        uniqueList.push(obj);
+      }
+    }  
+    return uniqueList;
+}
+
+
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { openUserPage } from "../controllers/user.controller.js";
+import { openUserPage, searchUser } from "../controllers/user.controller.js";
 import { searchBar } from "../controllers/users.controller.js";
 import { tokenvalidade } from "../middlewares/session.validaded.js";
 
 const userRouter = Router();
 
 userRouter.get("/user/:id", openUserPage);
-userRouter.get("/search", tokenvalidade, searchBar);
+// userRouter.get("/search", tokenvalidade, searchBar);
+userRouter.get("/search", tokenvalidade, searchUser);
 
 export default userRouter;
